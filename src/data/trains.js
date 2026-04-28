99cbf7919c07324bb6e4bc4234803e169e61d49a
godzifed <fadyntaras@gmail.com>
2026-04-28 21:52:25 +0300

    feat: add mock data for trains list

---
+++ src/data/trains.js
@@ -0,0 +1,30 @@
+// src/data/trains.js
+export const trainsData = [
+  {
+    id: 1,
+    trainNumber: "091К",
+    departureCity: "Київ",
+    arrivalCity: "Львів",
+    departureDate: "2023-11-25",
+    departureTime: "22:37",
+    duration: "07:23",
+  },
+  {
+    id: 2,
+    trainNumber: "705К",
+    departureCity: "Київ",
+    arrivalCity: "Перемишль",
+    departureDate: "2023-11-26",
+    departureTime: "17:00",
+    duration: "09:15",
+  },
+  {
+    id: 3,
+    trainNumber: "163О",
+    departureCity: "Харків",
+    arrivalCity: "Київ",
+    departureDate: "2023-11-25",
+    departureTime: "08:30",
+    duration: "06:45",
+  }
+];
